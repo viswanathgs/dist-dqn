05dcb61df842caabb43b79f88886daa5336f8ea8
Viswanath Sivakumar <viswanath@fb.com>
2016-07-05 17:29:51 -0700

    Update learning rate for CartPole

--- scripts/dqn_params.sh
+++ scripts/dqn_params.sh
@@ -6,14 +6,14 @@ CARTPOLE="\
 --env=CartPole-v0 \
 --network=simple \
 --optimizer=sgd \
---lr=0.0001 \
+--lr=0.001 \
 --minibatch_size=30 \
 --num_episodes=3000 \
 --max_steps_per_episode=200 \
 --replay_memory_capacity=10000 \
 --target_update_freq=300 \
 --reward_discount=0.9 \
---init_random_action_prob=0.5 \
+--init_random_action_prob=0.9 \
 --min_random_action_prob=0.1 \
 --random_action_prob_decay=0.99 \
 "
